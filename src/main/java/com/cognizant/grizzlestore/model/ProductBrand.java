450003055bcf8dbc8b4ade0a4f273b361ddc19d8
Ahmed Md Aziz <ahmed.mdaziz@cognizant.com>
2020-05-14 14:17:21 +0530

    init commit

---
+++ src/main/java/com/cognizant/grizzlestore/model/ProductBrand.java
@@ -0,0 +1,38 @@
+package com.cognizant.grizzlestore.model;
+
+public class ProductBrand {
+	
+	
+	private int brandId;
+	
+	private String brandName;
+	
+	
+	@Override
+	public String toString() {
+		return "ProductBrand [brandId=" + brandId + ", brandName=" + brandName + "]";
+	}
+	public int getBrandId() {
+		return brandId;
+	}
+	public void setBrandId(int brandId) {
+		this.brandId = brandId;
+	}
+	public String getBrandName() {
+		return brandName;
+	}
+	public void setBrandName(String brandName) {
+		this.brandName = brandName;
+	}
+	public ProductBrand() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public ProductBrand(int brandId, String brandName) {
+		super();
+		this.brandId = brandId;
+		this.brandName = brandName;
+	}
+	
+
+}
